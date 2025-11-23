ff665f00f3d91943bfc0851c53ddec01473d8cc9
vitorvhsilva <dndragonbr@gmail.com>
2025-11-23 10:49:58 -0300

    deploy java

--- SkillShift/src/services/contants.ts
+++ SkillShift/src/services/contants.ts
@@ -1,6 +1,6 @@
 //DEPLOY
 //export const TRILHAS_API = 'https://gs2-dotnet.onrender.com';
-//export const USUARIOS_API = "http://localhost:8080";
+//export const USUARIOS_API = "https://global-solution-java-e-iot.onrender.com";
 //MOCKSERVER 
 export const TRILHAS_API = "http://localhost:1080";
 export const USUARIOS_API = "http://localhost:1080";
